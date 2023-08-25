3c8a39c274c6145cc23b991fa7f7fec4e3e64c0a
AyomikeCharles <charlesayomike4@gmail.com>
2023-08-25 22:28:08 +0100

    added faqs

--- components/Faqs.tsx
+++ components/Faqs.tsx
@@ -60,7 +60,7 @@ const Faqs = () =>{
                                     </button>
                                 </div>
                             </div>
-                            <div style={{transition:'max-height 1s'}} className={`overflow-hidden transition duration-700 ease-out ${index === i ?'max-h-[200px]':'max-h-0'}`}>
+                            <div style={{transition:'max-height 1s'}} className={`overflow-hidden transition duration-700 ease-out ${index === i ?'max-h-[250px]':'max-h-0'}`}>
                                 {item.answers}
                             </div>
                         </div>
